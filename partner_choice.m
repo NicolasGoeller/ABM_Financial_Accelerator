c20dddd691a2c2d072bd259ad064abde909eb1fe
Nicolas <n.goeller@zeppelin-university.net>
2020-04-02 18:35:43 +0200

    worked on partner_choice function

--- partner_choice.m
+++ partner_choice.m
@@ -1,1 +1,23 @@
-function [pc] = partner_choice(interest, M)
+function [pc] = partner_choice(price, M, recipient, network, para)
+%Function return a vector of network positions that are selected based on
+%the offered prices. There is a tendency to stick with the current partner.
+% price: vector of prices offered up in the round
+% recipient
+pc = zeros(1, recipient);
+for i = 1:recipient
+    sub = zeros(2, M*length(price));
+    sub(1,:) = randsample(length(price), M*length(price));
+    sub(2,:) = price(sub(1,:));
+    
+    [cand, I] = min(sub(2,:));
+    ps = 0;
+    if cand < price(i)
+        ps = 1 - exp((para*(cand-price(i))/cand));
+    end
+    
+    if rand() < ps
+        pc(i) = sub(1,I);
+    else
+        pc(i) = network(i);
+    end
+end
--- test.m
+++ test.m
@@ -1,26 +1,9 @@
 clear
 
-T = 100;
-U = 5;
-D = 10;
-Au = ones(T,U);
-%Au(1,:) = randi([5, 20], 1, U);
+testp = randi([1 20], 1, 7)
+x = 0.5
+D = 5
 
-UD = zeros(T,D);
-UD(1,:) = randi([1 U],1,D);
-
-UD(1,:)
-Au(1,:)
-network_partners(Au(1,:), UD(1,:), D)
-
-
-UD = zeros(T,D);
-UD(1,:) = randi([1 U],1,D);
-
-for s = 1:3
-    size(UD(s,:))
-end
-
-for s = 1:T
-    s
-end
+[M,I] = min(testp)
+exp(1)
+%partner_choice(testp, x, D)
